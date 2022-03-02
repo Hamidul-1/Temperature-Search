0f6eeccad68e3b80ee1b44f8f6a74f4913634db6
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-02 20:39:45 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ const displayTemperature = temperature => {
     setInnerText('city',temperature.name);
     setInnerText('temperature',temperature.main.temp);
     setInnerText('condition',temperature.weather[0].main);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src',url);
 
     console.log(temperature);
 }
