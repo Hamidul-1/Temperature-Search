b2bbf924f2af20fc4b1ec1ac9d9b2f69b92c542e
Hamidul-1 <hamidul786cse@gmail.com>
2022-03-02 20:24:42 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const API_KEY = `a32f049b0b9ac96ba9fc1a21fe90712c`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city',temperature.name);
+    setInnerText('temperature',temperature.main.temp);
+    setInnerText('condition',temperature.weather[0].main);
+
+    console.log(temperature);
+}
